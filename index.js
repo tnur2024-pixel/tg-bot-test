771f4a317fbcd12e86c56716c7ab547d450ac7bf
tnur2024-pixel <tnur2024@gmail.com>
2026-01-04 12:56:28 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+import TelegramBot from "node-telegram-bot-api";
 import express from "express";
 
 const app = express();
 
-const PORT = process.env.PORT || 10000;
+// 🔑 তোমার BOT TOKEN এখানে বসাও
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
+const bot = new TelegramBot(BOT_TOKEN, { polling: true });
+
+// 👉 এটা খুব গুরুত্বপূর্ণ
+console.log("Bot started");
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Hello 👋 Bot is working!");
+});
+
+// Render server alive রাখার জন্য
+const PORT = process.env.PORT || 10000;
 app.get("/", (req, res) => {
   res.send("Server is running successfully 🚀");
 });
 
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log(`Server running on port ${PORT}`);
 });

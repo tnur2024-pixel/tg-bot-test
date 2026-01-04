0bbbabcd3854172bf2244ebc5b4449346a8785ee
tnur2024-pixel <tnur2024@gmail.com>
2026-01-04 12:05:51 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 10000;
+
 app.get("/", (req, res) => {
-  res.send("Server is running OK");
+  res.send("Server is running successfully 🚀");
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log("Server running on port", PORT);
 });
